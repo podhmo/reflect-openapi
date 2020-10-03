9dd9779eecb5c3d00be8cd0e7a69d8ac5c36e436
podhmo <ababjam61+github@gmail.com>
2020-10-04 04:39:15 +0900

    feat: Config.BuildDoc()

--- api.go
+++ api.go
@@ -25,3 +25,40 @@ func NewDocFromSkeleton(skeleton []byte) (*openapi3.Swagger, error) {
 	l := openapi3.NewSwaggerLoader()
 	return l.LoadSwaggerFromData(skeleton)
 }
+
+type Config struct {
+	Doc      *openapi3.Swagger
+	Resolver Resolver
+}
+
+func (c *Config) BuildDoc(use func(m *Manager)) (*openapi3.Swagger, error) {
+	if c.Resolver == nil {
+		c.Resolver = &UseRefResolver{}
+	}
+	if c.Doc == nil {
+		doc, err := NewDoc()
+		if err != nil {
+			return nil, err
+		}
+		c.Doc = doc
+	}
+
+	m := &Manager{
+		Doc:      c.Doc,
+		Resolver: c.Resolver,
+		Visitor:  NewVisitor(c.Resolver),
+	}
+	use(m)
+
+	if b, ok := c.Resolver.(Binder); ok {
+		b.Bind(m.Doc)
+	}
+	return m.Doc, nil
+}
+
+type Manager struct {
+	Visitor  *Visitor
+	Resolver Resolver
+
+	Doc *openapi3.Swagger
+}
