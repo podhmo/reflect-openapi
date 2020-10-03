6b43a97e8b114a459635c999dcb5d1ca0ab3bca3
podhmo <ababjam61+github@gmail.com>
2020-10-03 18:38:39 +0900

    docs: add github.com/labstack/echo example

---
+++ _examples/01echo/go.mod
@@ -0,0 +1,14 @@
+module m
+
+go 1.15
+
+replace github.com/podhmo/reflect-openapi => ../../
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/getkin/kin-openapi v0.22.1
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/podhmo/reflect-openapi v0.0.0-00010101000000-000000000000
+	golang.org/x/crypto v0.0.0-20201002170205-7f63de1d35b0 // indirect
+)
---
+++ _examples/01echo/main.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"strconv"
+
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	reflectopenapi "github.com/podhmo/reflect-openapi"
+)
+
+// simplified version of this.
+// https://swagger.io/docs/specification/basic-structure/
+
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+var (
+	users = []User{
+		{ID: 1, Name: "foo"},
+		{ID: 2, Name: "bar"},
+	}
+)
+
+// ListUsers returns a list of users.
+func ListUsers() []User {
+	return users
+}
+
+type GetUserInput struct {
+	UserID int `json:"userId" openapi:"path"`
+}
+
+// GetUser returns user
+func GetUser(input GetUserInput) (User, error) {
+	userID := input.UserID
+	for _, u := range users {
+		if u.ID == userID {
+			return u, nil
+		}
+	}
+	return User{}, fmt.Errorf("not found")
+}
+
+// ----------------------------------------
+type Router interface {
+	AddEndpoint(
+		method, path string,
+		interactor interface{},
+		handler echo.HandlerFunc,
+	)
+}
+
+type APIRouter struct {
+	Echo *echo.Echo
+}
+
+func (r *APIRouter) AddEndpoint(
+	method, path string,
+	interactor interface{},
+	handler echo.HandlerFunc,
+) {
+	r.Echo.Add(
+		method, path, handler,
+	)
+}
+
+type DocRouter struct {
+	Doc      *openapi3.Swagger
+	Resolver reflectopenapi.Resolver
+	Visitor  *reflectopenapi.Visitor
+}
+
+var (
+	rx = regexp.MustCompile(`:([^/:]+)`)
+)
+
+func (r *DocRouter) AddEndpoint(
+	method, path string,
+	interactor interface{},
+	handler echo.HandlerFunc,
+) {
+	oaPath := rx.ReplaceAllString(path, `{$1}`)
+	log.Println("replace path: ", path, "->", oaPath)
+	op := r.Visitor.VisitFunc(interactor)
+	r.Doc.AddOperation(oaPath, method, op)
+}
+
+func Mount(r Router) {
+	r.AddEndpoint(
+		"GET", "/users", ListUsers,
+		func(c echo.Context) error {
+			users := ListUsers()
+			return c.JSON(200, users)
+		},
+	)
+	r.AddEndpoint(
+		"GET", "/users/:userId", GetUser,
+		func(c echo.Context) error {
+			var input GetUserInput
+			{
+				v := c.Param("userId")
+				userID, err := strconv.ParseInt(v, 10, 0)
+				if err != nil {
+					return c.JSON(400, map[string]string{"message": err.Error()})
+				}
+				input.UserID = int(userID)
+			}
+
+			user, err := GetUser(input)
+			if err != nil {
+				return c.JSON(404, map[string]string{"message": err.Error()})
+			}
+			return c.JSON(200, user)
+		},
+	)
+}
+
+// ----------------------------------------
+
+func main() {
+	useDocF := flag.Bool("doc", false, "generate doc")
+	flag.Parse()
+
+	if err := run(*useDocF); err != nil {
+		log.Fatalf("%+v", err)
+	}
+}
+
+func run(useDoc bool) error {
+	addr := os.Getenv("ADDR")
+	if addr == "" {
+		addr = ":44444"
+	}
+
+	if useDoc {
+		log.Println("generate openapi doc")
+
+		doc, err := reflectopenapi.NewDoc()
+		if err != nil {
+			return err
+		}
+
+		r := &reflectopenapi.UseRefResolver{}
+		v := reflectopenapi.NewVisitor(r)
+
+		router := &DocRouter{
+			Resolver: r,
+			Visitor:  v,
+			Doc:      doc,
+		}
+		Mount(router)
+		r.Bind(doc)
+
+		enc := json.NewEncoder(os.Stdout)
+		enc.SetIndent("", "  ")
+		return enc.Encode(doc)
+	}
+
+	e := echo.New()
+	router := &APIRouter{Echo: e}
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	}))
+	Mount(router)
+
+	log.Println("listening ...", addr)
+	return http.ListenAndServe(addr, e)
+}
