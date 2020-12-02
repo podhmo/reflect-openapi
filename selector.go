fae9df210b84d24bbf4330e92316ab9380bbb474
podhmo <ababjam61+github@gmail.com>
2020-12-03 02:19:08 +0900

    feat: first prototype of merge params Selector

--- selector.go
+++ selector.go
@@ -1,15 +1,20 @@
 package reflectopenapi
 
-import "github.com/podhmo/reflect-openapi/pkg/shape"
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/podhmo/reflect-openapi/pkg/shape"
+)
 
 type DefaultSelector struct {
-	UseFirstInputSelector
-	UseFirstOutputSelector
+	FirstParamInputSelector
+	FirstParamOutputSelector
 }
 
-type UseFirstInputSelector struct{}
+type FirstParamInputSelector struct{}
 
-func (s *UseFirstInputSelector) SelectInput(fn shape.Function) shape.Shape {
+func (s *FirstParamInputSelector) SelectInput(fn shape.Function) shape.Shape {
 	if len(fn.Params.Values) == 0 {
 		return nil
 	}
@@ -22,9 +27,44 @@ func (s *UseFirstInputSelector) SelectInput(fn shape.Function) shape.Shape {
 	return nil
 }
 
-type UseFirstOutputSelector struct{}
+type MergeParamsInputSelector struct{}
 
-func (s *UseFirstOutputSelector) SelectOutput(fn shape.Function) shape.Shape {
+func (s *MergeParamsInputSelector) SelectInput(fn shape.Function) shape.Shape {
+	if len(fn.Params.Values) == 0 {
+		return nil
+	}
+
+	fields := shape.ShapeMap{}
+	tags := make([]reflect.StructTag, 0, fn.Params.Len())
+	metadata := make([]shape.FieldMetadata, 0, fn.Params.Len())
+	for i, p := range fn.Params.Values {
+		if p.GetFullName() == "context.Background" {
+			continue
+		}
+
+		name := fn.Params.Keys[i]
+		fields.Keys = append(fields.Keys, name)
+		fields.Values = append(fields.Values, p)
+		tags = append(tags, reflect.StructTag(fmt.Sprintf(`json:"%s"`, name))) // todo: handling customization
+		metadata = append(metadata, shape.FieldMetadata{})
+
+		// todo: handling required
+	}
+	return shape.Struct{
+		Info: &shape.Info{
+			Name:    "body", // rename?
+			Kind:    shape.Kind(reflect.Struct),
+			Package: fn.Info.Package,
+		},
+		Fields:   fields,
+		Tags:     tags,
+		Metadata: metadata,
+	}
+}
+
+type FirstParamOutputSelector struct{}
+
+func (s *FirstParamOutputSelector) SelectOutput(fn shape.Function) shape.Shape {
 	if len(fn.Returns.Values) == 0 {
 		return nil
 	}
