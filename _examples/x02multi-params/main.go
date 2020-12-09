22bef54e3495c9c1d93afd33437ba3d1d428a2de
podhmo <ababjam61+github@gmail.com>
2020-12-09 21:31:03 +0900

    chore: update examples

--- _examples/x02multi-params/main.go
+++ _examples/x02multi-params/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	reflectopenapi "github.com/podhmo/reflect-openapi"
 )
+
+type Greeter interface {
+	Greet(string)
+}
 
 func Add(x int, y int) int {
 	return x + y
@@ -17,6 +22,27 @@ func Hello(name string, pretty *bool) (string, error) {
 		return fmt.Sprintf("** Hello %s **", name), nil
 	}
 	return fmt.Sprintf("Hello %s", name), nil
+}
+func Hello2(ctx context.Context, g Greeter, name string, pretty *bool) (string, error) {
+	if *pretty {
+		return fmt.Sprintf("** Hello %s **", name), nil
+	}
+	return fmt.Sprintf("Hello %s", name), nil
+}
+
+type Person struct {
+	Name string `json:"name"`
+}
+
+func Hello3(subject *Person, object string, pretty *bool) (string, error) {
+	var prefix string
+	if subject != nil {
+		prefix = fmt.Sprintf("%s: ", subject.Name)
+	}
+	if *pretty {
+		return fmt.Sprintf("** %sHello %s **", prefix, object), nil
+	}
+	return fmt.Sprintf("%sHello %s", prefix, object), nil
 }
 func Sum(xs []int) int {
 	n := 0
@@ -36,7 +62,6 @@ func Sum2(xs ...int) int {
 func main() {
 	c := reflectopenapi.Config{
 		SkipValidation: true,
-		Resolver:       &reflectopenapi.NoRefResolver{},
 		Selector: &struct {
 			reflectopenapi.MergeParamsInputSelector
 			reflectopenapi.FirstParamOutputSelector
@@ -54,6 +79,14 @@ func main() {
 		{
 			op := m.Visitor.VisitFunc(Hello)
 			m.Doc.AddOperation("/Hello", "POST", op)
+		}
+		{
+			op := m.Visitor.VisitFunc(Hello2)
+			m.Doc.AddOperation("/Hello2", "POST", op)
+		}
+		{
+			op := m.Visitor.VisitFunc(Hello3)
+			m.Doc.AddOperation("/Hello3", "POST", op)
 		}
 		{
 			op := m.Visitor.VisitFunc(Sum)
