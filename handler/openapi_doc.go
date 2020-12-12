422867ceb1477ffb853bbb807e94c5c228d5dd3b
podhmo <ababjam61+github@gmail.com>
2020-12-12 19:53:13 +0900

    feat: openapi doc endpoint

---
+++ handler/openapi_doc.go
@@ -0,0 +1,20 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/getkin/kin-openapi/openapi3"
+)
+
+func OpenAPIDocHandler(doc *openapi3.Swagger) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		enc := json.NewEncoder(w)
+		if err := enc.Encode(doc); err != nil {
+			fmt.Fprintf(w, `{"error": %q}`, err.Error())
+			return
+		}
+	}
+}
