29ae6aef8dbc5367cf33ff00039a6b02d5913984
podhmo <ababjam61+github@gmail.com>
2021-06-15 07:22:38 +0900

    test: more tests (broken)

--- pkg/shape/format_test.go
+++ pkg/shape/format_test.go
@@ -1,6 +1,7 @@
 package shape_test
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"reflect"
@@ -56,6 +57,36 @@ func TestFormat(t *testing.T) {
 			stringFormat:       "io.WriteCloser",
 			valueFormat:        "io.WriteCloser{Close(), Write()}",
 			verboseValueFormat: "io.WriteCloser{Close(), Write()}", // TODO: type
+		},
+		{
+			msg: "container, map",
+			shape: func() shape.Shape {
+				var v = map[string]time.Time{}
+				return e.Extract(v)
+			}(),
+			stringFormat:       "map[string, time.Time]",
+			valueFormat:        "map[string, time.Time{wall, ext, loc}]",
+			verboseValueFormat: "map[string, time.Time{wall uint64, ext int64, loc *time.Location}]",
+		},
+		{
+			msg: "container, slice",
+			shape: func() shape.Shape {
+				var v = []*string{}
+				return e.Extract(v)
+			}(),
+			stringFormat:       "slice[*string]",
+			valueFormat:        "slice[*string]",
+			verboseValueFormat: "slice[*string]",
+		},
+		{
+			msg: "function",
+			shape: func() shape.Shape {
+				var v = func(ctx context.Context, s string, v interface{}) (shape.Shape, error) { return nil, nil }
+				return e.Extract(v)
+			}(),
+			stringFormat:       "github.com/podhmo/reflect-openapi/pkg/shape_test.TestFormat.func6.1(context.Context, string, ) (github.com/podhmo/reflect-openapi/pkg/shape.Shape, error)",
+			valueFormat:        "github.com/podhmo/reflect-openapi/pkg/shape_test.TestFormat.func6.1(context.Context{Deadline(), Done(), Err(), Value}, string, {}) (github.com/podhmo/reflect-openapi/pkg/shape.Shape{Clone(), GetFullName(), GetIdentity(), GetLv(), GetName(), GetPackage(), GetReflectKind(), GetReflectType(), GetReflectValue(), ResetName(), ResetPackage(), ResetReflectType(), Shape(), deref(), info}, error{Error})",
+			verboseValueFormat: "github.com/podhmo/reflect-openapi/pkg/shape_test.TestFormat.func6.1(context.Context{Deadline(), Done(), Err(), Value}, string, {}) (github.com/podhmo/reflect-openapi/pkg/shape.Shape{Clone(), GetFullName(), GetIdentity(), GetLv(), GetName(), GetPackage(), GetReflectKind(), GetReflectType(), GetReflectValue(), ResetName(), ResetPackage(), ResetReflectType(), Shape(), deref(), info}, error{Error})",
 		},
 	}
 
