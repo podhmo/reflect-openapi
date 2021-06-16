f65d4a203374a088f4437410569c0316cd5181f5
podhmo <ababjam61+github@gmail.com>
2021-06-16 13:13:34 +0900

    Null string examples (#63)
    * chore: add null-string example
    
    * chore: add Title
    
    * chore: update go.sum
    
    * chore: update examples
    
    * test: fix tests

---
+++ _examples/x03null-string/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/guregu/null"
+	reflectopenapi "github.com/podhmo/reflect-openapi"
+)
+
+type Person struct {
+	ID        string      `json:"id" required:"true"`
+	Name      string      `json:"name"`
+	NickName  null.String `json:"nickname"`
+	NickName2 null.String `json:"nickname2"`
+}
+
+func main() {
+	c := reflectopenapi.Config{
+		SkipValidation: true,
+	}
+	if ok, _ := strconv.ParseBool(os.Getenv("WITHOUT_REF")); ok {
+		c.Resolver = &reflectopenapi.NoRefResolver{}
+	}
+	c.EmitDoc(func(m *reflectopenapi.Manager) {
+		installNullable(m)
+		{
+			m.Visitor.VisitType(Person{})
+		}
+	})
+}
+
+func installNullable(m *reflectopenapi.Manager) {
+	{
+		var v null.Bool
+		m.Visitor.VisitType(v, func(schema *openapi3.Schema) {
+			schema.Title = "Null" + schema.Title
+			schema.Nullable = true
+			schema.Properties = nil
+		})
+	}
+	{
+		var v null.Float
+		m.Visitor.VisitType(v, func(schema *openapi3.Schema) {
+			schema.Title = "Null" + schema.Title
+			schema.Nullable = true
+			schema.Properties = nil
+		})
+	}
+	{
+		var v null.Int
+		m.Visitor.VisitType(v, func(schema *openapi3.Schema) {
+			schema.Title = "Null" + schema.Title
+			schema.Type = "integer"
+			schema.Nullable = true
+			schema.Properties = nil
+
+		})
+	}
+	{
+		var v null.String
+		m.Visitor.VisitType(v, func(schema *openapi3.Schema) {
+			schema.Title = "Null" + schema.Title
+			schema.Type = "string"
+			schema.Nullable = true
+			schema.Properties = nil
+		})
+	}
+	// {
+	// 	var v null.Time
+	// 	m.Visitor.VisitType(v, func(schema *openapi3.Schema) {
+	// 		schema.Nullable = true
+	// 	})
+	// }
+}
