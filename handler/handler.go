a86cf6deac0167cecf1f35746550a4a920e16705
podhmo <ababjam61+github@gmail.com>
2022-07-18 10:11:30 +0900

    chore: adjust logging message

--- handler/handler.go
+++ handler/handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"log"
 	"net/http"
 	"strings"
 
@@ -10,6 +11,10 @@ import (
 func NewHandler(doc *openapi3.T, basePath string) http.Handler {
 	mux := &http.ServeMux{}
 	basePath = strings.TrimSuffix(basePath, "/")
+
+	for _, server := range doc.Servers {
+		log.Printf("[INFO]  openapi-doc handler is registerd in GET %s%s/", server.URL, basePath)
+	}
 
 	mux.Handle(basePath+"/", ListEndpointHandler(
 		doc,
