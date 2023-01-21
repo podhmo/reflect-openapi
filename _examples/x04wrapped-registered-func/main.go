8e642181975621f777b3a0d6a8c4d78dde397fa3
podhmo <ababjam61+github@gmail.com>
2023-01-21 21:12:52 +0900

    chore: update example (ng)

--- _examples/x04wrapped-registered-func/main.go
+++ _examples/x04wrapped-registered-func/main.go
@@ -7,24 +7,32 @@ import (
 	reflectopenapi "github.com/podhmo/reflect-openapi"
 )
 
+// Action is wrapped type for wrapping function something like DefineGet()
 type Action[I any, O any] func(ctx context.Context, input I) (O, error)
 
+// Foo obejct
 type Foo struct {
 	ID   int
-	Name string
+	Name string // name of foo
 }
 
+// GetFoo's input parameters (ignored)
 type GetFooInput struct {
-	ID int `in:"path" path:"id"`
+	ID int `in:"path" path:"id"` // id of foo
 }
 
+// GetFoo returns matched foo object.
 func GetFoo(ctx context.Context, input GetFooInput) (*Foo, error) { return nil, nil }
 
 func main() {
 	c := reflectopenapi.Config{}
 	c.EmitDoc(func(m *reflectopenapi.Manager) {
-		m.RegisterFunc(GetFoo).After(func(op *openapi3.Operation) {
-			m.Doc.AddOperation("/foo/{id}", "GET", op)
-		})
+		DefineGet(m, "/foo/{id}", GetFoo)
+	})
+}
+
+func DefineGet[I any, O any](m *reflectopenapi.Manager, path string, action Action[I, O]) {
+	m.RegisterFunc(action).After(func(op *openapi3.Operation) {
+		m.Doc.AddOperation(path, "GET", op)
 	})
 }
