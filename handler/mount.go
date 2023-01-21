13ed78a77337e63113d852caa99e0faf1d1c7f19
podhmo <ababjam61+github@gmail.com>
2023-01-21 14:34:30 +0900

    fix: add redoc handler info

--- handler/mount.go
+++ handler/mount.go
@@ -20,6 +20,7 @@ func NewHandler(doc *openapi3.T, basePath string) http.Handler {
 		doc,
 		Endpoint{Method: "GET", Path: basePath + "/doc", OperationID: "OpenAPIDocHandler", Summary: "(added by github.com/podhmo/reflect-openapi/handler)"},
 		Endpoint{Method: "GET", Path: basePath + "/ui", OperationID: "SwaggerUIHandler", Summary: "(added by github.com/podhmo/reflect-openapi/handler)"},
+		Endpoint{Method: "GET", Path: basePath + "/redoc", OperationID: "RedocHandler", Summary: "(added by github.com/podhmo/reflect-openapi/handler)"},
 	))
 	mux.Handle(basePath+"/doc", OpenAPIDocHandler(doc))
 	mux.Handle(basePath+"/ui", SwaggerUIHandler(doc, basePath))
