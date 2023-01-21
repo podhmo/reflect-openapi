29c74e5000cd3487920fec63cc6fc7cf900a642c
podhmo <ababjam61+github@gmail.com>
2023-01-21 23:29:47 +0900

    chore: add custom-type example

---
+++ _examples/x05custom-type/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/getkin/kin-openapi/openapi3"
+	reflectopenapi "github.com/podhmo/reflect-openapi"
+)
+
+type Name string
+
+type Article struct {
+	Title string
+	Owner Name
+}
+
+func main() {
+	c := &reflectopenapi.Config{}
+	c.EmitDoc(func(m *reflectopenapi.Manager) {
+		m.RegisterType(Name(""), func(schema *openapi3.Schema) {
+			schema.Pattern = `^[A-Z][A-Za-z\-]+$`
+			schema.Description = "Name of something"
+		})
+		m.RegisterType(Article{})
+	})
+}
