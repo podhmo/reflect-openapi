31c440406aa4dd83bb0da17d0a38c16098c00f32
podhmo <ababjam61+github@gmail.com>
2023-02-11 19:16:31 +0900

    test: add tests

--- docgen/docgen.go
+++ docgen/docgen.go
@@ -4,8 +4,6 @@ import (
 	"embed"
 	"fmt"
 	"io"
-	"regexp"
-	"strings"
 	"text/template"
 
 	"github.com/getkin/kin-openapi/openapi3"
@@ -34,50 +32,6 @@ type Endpoint struct {
 type DocumentInfo struct {
 	Summary     string
 	Description string
-}
-
-var (
-	TRUNCATE_SIZE = 88
-)
-
-func toDocumentInfo(summary, description string) (di DocumentInfo) {
-	// fmt.Fprintf(os.Stderr, "summary: %q\n", summary)
-	// fmt.Fprintf(os.Stderr, "description: %q\n", description)
-	// fmt.Fprintf(os.Stderr, "--\n")
-
-	defer func() {
-		if len(di.Summary) > TRUNCATE_SIZE {
-			di.Summary = di.Summary[:TRUNCATE_SIZE]
-		}
-	}()
-
-	parts := strings.Split(description, "\n")
-	if len(parts) > 2 && strings.TrimSpace(parts[1]) == "" {
-		di.Summary = strings.TrimSpace(parts[0])
-		di.Description = strings.TrimSpace(strings.Join(parts[2:], "\n"))
-		return
-	} else if summary != "" {
-		di.Summary = strings.TrimSpace(summary)
-		di.Description = strings.TrimSpace(description)
-		return
-	} else {
-		di.Summary = strings.TrimSpace(parts[0])
-		di.Description = strings.TrimSpace(strings.Join(parts[1:], "\n"))
-		return
-	}
-}
-
-var (
-	toDashRegex  = regexp.MustCompile(`[ \t]+`)
-	toEmptyRegex = regexp.MustCompile(`[{/\.}]+`)
-)
-
-func toHtmlID(operationID, method, path string) string {
-	s := fmt.Sprintf("%s %s %s", operationID, method, path)
-	s = strings.ToLower(s)
-	s = toEmptyRegex.ReplaceAllString(s, "")
-	s = toDashRegex.ReplaceAllString(s, "-")
-	return s
 }
 
 func Generate(doc *openapi3.T) *Doc {
