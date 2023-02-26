779c6c9ba703901d7c61c531a5a99cb4561e6641
podhmo <ababjam61+github@gmail.com>
2023-02-26 19:40:24 +0900

    chore: update example

--- _examples/x07pagination/main.go
+++ _examples/x07pagination/main.go
@@ -1,17 +1,52 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/getkin/kin-openapi/openapi3"
 	reflectopenapi "github.com/podhmo/reflect-openapi"
+	"github.com/podhmo/reflect-openapi/docgen"
+	"github.com/podhmo/reflect-openapi/info"
 )
 
 func main() {
-	c := &reflectopenapi.Config{}
-	c.EmitDoc(func(m *reflectopenapi.Manager) {
+	if err := run(); err != nil {
+		log.Fatalf("!! %+v", err)
+	}
+}
+
+func run() error {
+	c := &reflectopenapi.Config{Info: info.New()}
+	ctx := context.Background()
+	doc, err := c.BuildDoc(ctx, func(m *reflectopenapi.Manager) {
 		m.RegisterFunc(ListUser, func(op *openapi3.Operation) {
 			m.Doc.AddOperation("/users", "GET", op)
 		})
 	})
+	if err != nil {
+		return fmt.Errorf("build doc: %w", err)
+	}
+
+	enc := json.NewEncoder(os.Stdout)
+	enc.SetIndent("", "  ")
+	if err := enc.Encode(doc); err != nil {
+		return fmt.Errorf("write doc: %w", err)
+	}
+
+	md := docgen.Generate(doc, c.Info)
+	f, err := os.Create("README.md")
+	if err != nil {
+		return fmt.Errorf("new md: %w", err)
+	}
+	defer f.Close()
+	if err := docgen.WriteDoc(f, md); err != nil {
+		return fmt.Errorf("write md: %w", err)
+	}
+	return nil
 }
 
 type PaginatedInput[T any] struct {
