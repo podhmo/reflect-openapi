c1f9140aa27634639480c24d4240e1b80ac92bfc
podhmo <ababjam61+github@gmail.com>
2023-02-26 19:02:46 +0900

    chore: add first example

---
+++ _examples/x07pagination/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/getkin/kin-openapi/openapi3"
+	reflectopenapi "github.com/podhmo/reflect-openapi"
+)
+
+func main() {
+	c := &reflectopenapi.Config{}
+	c.EmitDoc(func(m *reflectopenapi.Manager) {
+		m.RegisterFunc(ListUser, func(op *openapi3.Operation) {
+			m.Doc.AddOperation("/users", "GET", op)
+		})
+	})
+}
+
+type PaginatedInput[T any] struct {
+	Cursor   string `in:"query" query:"cursor"`
+	PageSize int    `in:"query" query:"pageSize"`
+
+	Value T `json:"value" embedded:"true"` // TODO: need embedded in generics
+}
+
+type PaginatedOutput[T any] struct {
+	HasMore    bool   `json:"hasMore"`
+	Cursor     string `json:"cursor"`
+	NextCursor string `json:"nextCursor"`
+
+	Items T `json:"items"`
+}
+
+type User struct {
+	Name string `json:"name"`
+}
+
+type ListUserInput struct {
+	Sort  string `in:"query" query:"sort" openapi-override:"{'enum': ['asc', 'desc'], 'default': 'desc'}"`
+	Query string `in:"query" query:"query"`
+}
+
+func ListUser(input PaginatedInput[ListUserInput]) (*PaginatedOutput[[]User], error) {
+	return nil, nil
+}
