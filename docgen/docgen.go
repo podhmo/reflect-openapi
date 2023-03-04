6cdbb19b63dec11a7daf1e1b7fbc864d8276e0b6
podhmo <ababjam61+github@gmail.com>
2023-03-04 19:22:39 +0900

    refactor: use internal function

--- docgen/docgen.go
+++ docgen/docgen.go
@@ -84,18 +84,7 @@ func Generate(doc *openapi3.T, info *info.Info) *Doc {
 				if body.Value != nil { // not support request component
 					media := body.Value.Content.Get("application/json")
 					if media != nil {
-						schema := info.LookupSchema(media.Schema)
-						typ := schema.Title
-						switch schema.Type {
-						case openapi3.TypeArray:
-							schema = info.LookupSchema(schema.Items)
-							typ = "[]" + schema.Title
-						case openapi3.TypeObject:
-							if schema.AdditionalProperties.Schema != nil {
-								schema = info.LookupSchema(schema.Items)
-								typ = "map[string]" + schema.Title
-							}
-						}
+						schema, typ := toInnerSchemaAndTypeExpr(info, media.Schema)
 						if sinfo, ok := info.SchemaInfo[schema]; ok {
 							// log.Printf("[DEBUG] schema link: %q link input of %q", typ, op.OperationID)
 							sinfo.Links = append(sinfo.Links, Link{Title: fmt.Sprintf("input of %s as `%s`", op.OperationID, typ), URL: "#" + htmlID})
@@ -111,18 +100,7 @@ func Generate(doc *openapi3.T, info *info.Info) *Doc {
 						output := Object{Name: name, TypeString: ActionOutputString(doc, info, ref, name), ContentType: contentType}
 						switch contentType {
 						case "application/json":
-							schema := info.LookupSchema(media.Schema)
-							typ := schema.Title
-							switch schema.Type {
-							case openapi3.TypeArray:
-								schema = info.LookupSchema(schema.Items)
-								typ = "[]" + schema.Title
-							case openapi3.TypeObject:
-								if schema.AdditionalProperties.Schema != nil {
-									schema = info.LookupSchema(schema.Items)
-									typ = "map[string]" + schema.Title
-								}
-							}
+							schema, typ := toInnerSchemaAndTypeExpr(info, media.Schema)
 							if sinfo, ok := info.SchemaInfo[schema]; ok {
 								// log.Printf("[DEBUG] schema link: %q link output of %q (%s)", typ, op.OperationID, name)
 								sinfo.Links = append(sinfo.Links, Link{Title: fmt.Sprintf("output of %s (%s) as `%s`", op.OperationID, name, typ), URL: "#" + htmlID})
@@ -225,4 +203,20 @@ func WriteDoc(w io.Writer, doc *Doc) error {
 		return fmt.Errorf("write doc: %w", err)
 	}
 	return nil
+}
+
+func toInnerSchemaAndTypeExpr(info *info.Info, ref *openapi3.SchemaRef) (*openapi3.Schema, string) {
+	schema := info.LookupSchema(ref)
+	typ := schema.Title
+	switch schema.Type {
+	case openapi3.TypeArray:
+		schema = info.LookupSchema(schema.Items)
+		typ = "[]" + schema.Title
+	case openapi3.TypeObject:
+		if schema.AdditionalProperties.Schema != nil {
+			schema = info.LookupSchema(schema.Items)
+			typ = "map[string]" + schema.Title
+		}
+	}
+	return schema, typ
 }
